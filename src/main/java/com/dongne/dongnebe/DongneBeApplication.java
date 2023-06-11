f0ec3220bc9784f5558e5f772fe7334e1bd31f7e
cms <minoui1234@gmail.com>
2023-06-11 18:10:49 +0900

    first commit

---
+++ src/main/java/com/dongne/dongnebe/DongneBeApplication.java
@@ -0,0 +1,13 @@
+package com.dongne.dongnebe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DongneBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DongneBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dongne/dongnebe/DongneBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dongne.dongnebe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DongneBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
