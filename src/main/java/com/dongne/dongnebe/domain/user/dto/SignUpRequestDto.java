b7825a908f949a48662d647d5aa70b92f10c84ca
cms <minoui1234@gmail.com>
2023-07-01 00:49:29 +0900

    [fix] requestbody dto 에 기본 생성자 추가

--- src/main/java/com/dongne/dongnebe/domain/user/dto/SignUpRequestDto.java
+++ src/main/java/com/dongne/dongnebe/domain/user/dto/SignUpRequestDto.java
@@ -1,10 +1,14 @@
 package com.dongne.dongnebe.domain.user.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class SignUpRequestDto {
     private String userId;
     private String username;
