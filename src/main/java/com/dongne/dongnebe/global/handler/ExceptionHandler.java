9393012fc49339700e475dc58c1435f7f80e51ff
cms <minoui1234@gmail.com>
2023-07-01 00:49:03 +0900

    [feat] ExceptionHandler 추가

--- src/main/java/com/dongne/dongnebe/global/handler/ExceptionHandler.java
+++ src/main/java/com/dongne/dongnebe/global/handler/ExceptionHandler.java
@@ -1,10 +1,13 @@
 package com.dongne.dongnebe.global.handler;
 
 import com.dongne.dongnebe.global.dto.ErrorResponseDto;
+import com.dongne.dongnebe.global.exception.user.IncorrectPasswordException;
+import com.dongne.dongnebe.global.exception.user.NicknameAlreadyExistException;
 import com.dongne.dongnebe.global.exception.user.UserIdAlreadyExistException;
 import com.dongne.dongnebe.global.exception.user.UserIdNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
@@ -15,7 +18,22 @@ public class ExceptionHandler {
     }
 
     @org.springframework.web.bind.annotation.ExceptionHandler
-    public ResponseEntity<ErrorResponseDto> userIdAlreadyExistHandler(UserIdAlreadyExistException exception) {
+    public ResponseEntity<ErrorResponseDto> userIdAlreadyExistExceptionHandler(UserIdAlreadyExistException exception) {
+        return new ResponseEntity<>(ErrorResponseDto.badRequest(exception.getMessage()), HttpStatus.BAD_REQUEST);
+    }
+
+    @org.springframework.web.bind.annotation.ExceptionHandler
+    public ResponseEntity<ErrorResponseDto> incorrectPasswordExceptionHandler(IncorrectPasswordException exception) {
+        return new ResponseEntity<>(ErrorResponseDto.badRequest(exception.getMessage()), HttpStatus.BAD_REQUEST);
+    }
+
+    @org.springframework.web.bind.annotation.ExceptionHandler
+    public ResponseEntity<ErrorResponseDto> nicknameAlreadyExistExceptionHandler(NicknameAlreadyExistException exception) {
+        return new ResponseEntity<>(ErrorResponseDto.badRequest(exception.getMessage()), HttpStatus.BAD_REQUEST);
+    }
+
+    @org.springframework.web.bind.annotation.ExceptionHandler
+    public ResponseEntity<ErrorResponseDto> methodArgumentNotValidExceptionHandler(MethodArgumentNotValidException exception) {
         return new ResponseEntity<>(ErrorResponseDto.badRequest(exception.getMessage()), HttpStatus.BAD_REQUEST);
     }
 }
